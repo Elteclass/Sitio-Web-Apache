3b0fc8fdf73a45799811989ffe679e8965e9e624
Jaime Antonio Alvarez Crisóstomo <126440030+Elteclass@users.noreply.github.com>
2025-05-06 08:07:47 -0700

    Add files via upload

---
+++ miSitioWeb/JS/code.js
@@ -0,0 +1,9 @@
+//SHITF + ALT + FLECHA
+//CTRL + ALT + FLECHA 
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x')
+    navbar.classList.toggle('open');
+}
